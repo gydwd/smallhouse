d22243606f66be191d909ee78ca52e5dcd473ef2
guoyangdu <yangdu.guo@yi23.net>
2021-04-24 17:50:14 +0800

    first commit

---
+++ src/main/java/com/gmanba/smallhouse/SmallhouseApplication.java
@@ -0,0 +1,13 @@
+package com.gmanba.smallhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmanba/smallhouse/SmallhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmanba.smallhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
